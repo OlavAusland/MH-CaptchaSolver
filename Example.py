55dd3cf2238d5cedaddb09a0f5586aa221f68a45
Olav Ausland Onstad <olavausland@hotmail.com>
2023-10-12 18:01:22 +0200

    Created a plug & play repository with only the models, example annotation images and a example python file to run the models & predict these images

--- Example.py
+++ Example.py
@@ -1,15 +1,12 @@
 import os
-
 import cv2
 import keras.models
-import tensorflow as tf
-import numpy as np
 
 INPUT_SHAPE = (96, 71, 3)
 CHAR_SIZE = (20, 30)
 cv2.namedWindow('prediction', cv2.WINDOW_NORMAL)
 
-bbox_model: keras.models.Model = keras.models.load_model('./rgb_model.h5')
+bbox_model: keras.models.Model = keras.models.load_model('./bbox_model.h5')
 char_model: keras.models.Model = keras.models.load_model('./character_model.h5')
 
 
@@ -17,7 +14,7 @@ def predict_image(path: str):
     original_image = cv2.imread(path) / 255.0
     original_image = original_image[12:71 + 12, 7:96 + 7]
     image = original_image.copy()
-    print(image.shape)
+
     predictions = bbox_model.predict([image.reshape(-1, 96, 71, 3)])
     for prediction in predictions:
         x_min: int = int(prediction[0][0] * INPUT_SHAPE[0])
@@ -28,8 +25,7 @@ def predict_image(path: str):
         cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (255, 255, 255), 1)
 
         character = original_image[y_min+1:y_max, x_min+1:x_max]
-        cv2.imwrite('./original_image.png', original_image)
-        cv2.imwrite('./test.png', image[y_min+1:y_max, x_min+1:x_max]*255.0)
+
         character = cv2.resize(character, dsize=(20, 30), interpolation=cv2.INTER_AREA)
         character_prediction = char_model.predict([character.reshape(-1, 20, 30, 3)])
 
